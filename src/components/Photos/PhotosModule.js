dd54f8ad9c19de6039376011ad7dc89fb7d4dd60
neocydfr <franckboudraa@gmail.com>
2018-03-13 15:11:39 +0100

    improved homepage portal style

---
+++ src/components/Photos/PhotosModule.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+const PhotosModule = () => {
+  return <Segment>PhotosModule</Segment>;
+};
+
+export default PhotosModule;
--- src/components/StaticPages/Homepage.js
+++ src/components/StaticPages/Homepage.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Image } from 'semantic-ui-react';
 
 import Countdown from '../Countdown/Countdown';
 import CarouselPlayer from '../Carousel/CarouselPlayer';
-import ArticleList from '../Articles/ArticleList';
+import ArticleModule from '../Articles/ArticleModule';
+import PhotosModule from '../Photos/PhotosModule';
+
+import motivationCover from '../../assets/motivation_cover.jpg';
 
 const Homepage = () => {
   return (
-    <Container>
+    <Container className="mb-2">
       <CarouselPlayer />
-      <Grid stackable padded="vertically">
-        <Grid.Row stretched columns={2}>
+      <Grid stackable>
+        <Grid.Row stretched columns={2} className="pb-1 pt-4">
           <Grid.Column width={6} className="pr-1">
-            <ArticleList />
+            <ArticleModule />
           </Grid.Column>
-          <Grid.Column width={10} className="pl-3">
+          <Grid.Column width={10} className="pl-1">
             <Countdown />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched columns={3} className="pt-1">
+          <Grid.Column className="pr-1" mobile={16} tablet={6} computer={6}>
+            <PhotosModule />
+          </Grid.Column>
+          <Grid.Column className="px-1" mobile={16} tablet={6} computer={6}>
+            <PhotosModule />
+          </Grid.Column>
+          <Grid.Column className="pl-1" mobile={16} tablet={4} computer={4}>
+            <Image
+              src={motivationCover}
+              as="img"
+              style={{ imageFit: 'cover' }}
+            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
